98c9798b31620dfa8e23378aa040c838c3c27532
Tim Pushor <tim.pushor@gmail.com>
2021-12-27 12:13:58 -0700

    Add main content

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import "./App.css";
+import React from 'react';
 import Header from './Components/Header/Header';
+import MainContent from './Components/MainContent/MainContent';
+
+import "./App.css";
 
 function App() {
-  return <Header />;
+  return <React.Fragment>
+  <Header />
+  <MainContent />
+  </React.Fragment>
 }
 
 export default App;
---
+++ src/Components/MainContent/MainContent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './MainContent.module.css';
+
+const MainContent = () => {
+    return <section className={styles.content}>
+        <div className={styles['content-left']}>
+
+        </div>
+        <div className={styles['content-right']}>
+
+        </div>
+    </section>
+};
+
+export default MainContent;
+
