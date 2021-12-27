28300d4807fd25f4d490c1268b2e401e5a09f663
Tim Pushor <tim.pushor@gmail.com>
2021-12-27 11:49:20 -0700

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import Header from './Components/Header';
 
 function App() {
-  return <div className="App"></div>;
+  return <Header />;
 }
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Header.module.css';
+import headerImage from '../assets/logo.png';
+
+const Header = () => {
+    return <header className={styles.header}>
+        <img src={headerImage} alt='Logo' />
+        <h1>Metabolic Rate Calculator</h1>
+    </header>
+}
+
+export default Header;
