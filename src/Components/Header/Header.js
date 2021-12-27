308cd8f5f05fe93534fc3bfcda8ae53e68d228dc
Tim Pushor <tim.pushor@gmail.com>
2021-12-27 12:00:44 -0700

    Refactor header into its own folder

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import styles from './Header.module.css';
-import headerImage from '../assets/logo.png';
+import headerImage from '../../assets/logo.png';
 
 const Header = () => {
     return <header className={styles.header}>
